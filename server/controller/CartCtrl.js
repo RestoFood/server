5f52d5225c25491c55893a91b08cee011033d907
JenabB <birulangit1996@gmail.com>
2021-10-25 14:41:38 +0700

    addtocart

--- server/controller/CartCtrl.js
+++ server/controller/CartCtrl.js
@@ -14,30 +14,58 @@ const saveLineItems = async (
   reme_price,
   cart_id
 ) => {
-  return await req.context.models.cart_line_items.create({
+  const isAlreadyExist = await req.context.models.cart_line_items.findOne({
+    where: { clit_reme_id: reme_id },
+  });
+
+  if (isAlreadyExist) {
+    const { clit_id, clit_qty } =
+      await req.context.models.cart_line_items.findOne({
+        where: { clit_reme_id: reme_id },
+      });
+    const newQty = clit_qty + qty;
+    const updateLineItems = await req.context.models.cart_line_items.update(
+      {
+        clit_qty: newQty,
+        clit_price: reme_price,
+        clit_subtotal: newQty * reme_price,
+      },
+      { returning: true, where: { clit_id: clit_id } }
+    );
+    return updateLineItems;
+  }
+
+  const newLineItems = await req.context.models.cart_line_items.create({
     clit_reme_id: reme_id,
     clit_redon_id: redon_id,
     clit_qty: qty,
     clit_price: reme_price,
     clit_subtotal: qty * reme_price,
-    clit_order_name: "",
     clit_cart_id: cart_id,
   });
+
+  return newLineItems;
 };
 
 const addToCart = async (req, res, next) => {
-  const { reme_id, redon_id, qty, user_id } = req.body;
+  const { reme_id, redon_id, qty } = req.body;
+  const { userId } = req.user;
 
   try {
-    const isCartExist = await req.context.models.carts.findOne({
-      where: { cart_user_id: user_id },
+    const { reme_reto_id, reme_price } =
+      await req.context.models.resto_menu.findOne({
+        where: { reme_id: reme_id },
+      });
+    const isCartOpen = await req.context.models.carts.findOne({
+      where: {
+        cart_status: "open",
+        cart_reto_id: reme_reto_id,
+        cart_user_id: userId,
+      },
     });
-    if (!isCartExist) {
-      const { reme_reto_id, reme_price } =
-        await req.context.models.resto_menu.findOne({
-          where: { reme_id: reme_id },
-        });
-      const cart = await createCart(req, reme_reto_id, user_id);
+
+    if (!isCartOpen) {
+      const cart = await createCart(req, reme_reto_id, userId);
       const { cart_id } = cart.dataValues;
 
       const lineItems = await saveLineItems(
@@ -49,9 +77,25 @@ const addToCart = async (req, res, next) => {
         cart_id
       );
       return res.send(lineItems);
-    }
+    } else {
+      const { cart_id } = await req.context.models.carts.findOne({
+        where: {
+          cart_status: "open",
+          cart_reto_id: reme_reto_id,
+          cart_user_id: userId,
+        },
+      });
 
-    return res.send("sudah ada");
+      const addToNewLineItems = await saveLineItems(
+        req,
+        reme_id,
+        redon_id,
+        qty,
+        reme_price,
+        cart_id
+      );
+      return res.send(addToNewLineItems);
+    }
   } catch (error) {
     console.error(error);
     return res.send(error);
